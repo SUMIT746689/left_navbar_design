c415caf5cf0eac73c1774d990028218a7f098a58
Mehedi Hasan Sumit <72936930+SUMIT746689@users.noreply.github.com>
2023-01-15 10:38:25 +0600

    fix typescript

--- src/app/navdropdown.tsx
+++ src/app/navdropdown.tsx
@@ -1,18 +1,19 @@
 'use client';
 
-import { FC, useEffect, useRef } from "react"
+import React, { FC, useEffect, useRef } from "react"
 
 interface NAVINTER {
+  click: boolean;
   children: any;
 }
 
-export default function ({ click, children }) {
+export default function ({ click, children }: NAVINTER) {
 
   const ref: any = useRef([]);
 
   const data = children.map((child: any, index: number) => {
     return <div
-      style={{ height: '0px', padding: '0px', backgroundColor: '#c084fc', transitionDuration: '500ms',overflow:'hidden' }}
+      style={{ height: '0px', padding: '0px', backgroundColor: '#c084fc', transitionDuration: '500ms', overflow: 'hidden' }}
       key={index}
       ref={(element) => { ref.current[index] = element }}
     >
@@ -32,7 +33,7 @@ export default function ({ click, children }) {
         if (!click) r.style.padding = '0';
         if (!click) r.style.transform = 'translateX(-100%)';
         if (!click) r.style.borderBottom = 'none';
-        
+
         if (click) r.style.height = '40px';
         if (click) r.style.opacity = '1';
         if (click) r.style.padding = '8px';
